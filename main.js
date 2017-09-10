4417fca719ad5c1ab21b9161b431390309143615
Vincent Nguyen <atermagus312@gmail.com>
2017-09-10 14:11:16 -0700

    follow request

--- main.js
+++ main.js
@@ -59,4 +59,7 @@ app.on('activate', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 var login = require('./lib/login.js'); 
-login.init(); 
+var ws = login.init(); 
+
+var followers = require('./lib/followers.js'); 
+var follows = followers.init((data) => { console.log(data)}); 
